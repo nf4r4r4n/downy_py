1af92ed6a0583d1651ed4ceb731b3167c7f4400a
nf4r4r4n <baibs.fararano@gmail.com>
2026-05-29 16:52:17 +0300

    feat: downloader is finally done with postprocessing

--- core/dowloader.py
+++ core/dowloader.py
@@ -9,11 +9,27 @@ class Downloader:
         self._config = config
 
     def _build_opts(self) -> dict:
-        return {
-            "outtmpl": "%(title)s.%(ext)s",
+        opts = {
+            "outtmpl": "downloads/%(title)s.%(ext)s",
             "quiet": False,
             "noprogress": False
         }
+
+        if self._config["media_type"] == "audio":
+            opts["format"] = "bestaudio/best"
+            opts["postprocessors"] = [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": self._config["format"],
+                    "preferredquality": "192"
+                }
+            ]
+        else:
+            opts["format"] = "bestvideo+bestaudio/best"
+            opts["merge_output_format"] = self._config["format"]
+            opts["postprocessors"] = []
+
+        return opts
 
     def download(self):
         opts = self._build_opts()
